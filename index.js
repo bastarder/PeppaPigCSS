03e6d310fa51ac531aa31ca7ec89ebecb3fcacdb
bastarder <jie.qian@blockheaders.com>
2018-07-23 22:27:43 +0800

    del click event

--- index.js
+++ index.js
@@ -2,13 +2,7 @@ window.onload = function(){
   const $ = document.querySelector.bind(document);
   let pig = $('.draw');
   let head = $('.head');
-
-  head.addEventListener('click', () => {
-    const { transform : transformStr } = head.style.transform;
-
-    head.style.transform = ~transformStr.indexOf("rotateY(180deg)") ? "rotateY(0deg)" : "rotateY(180deg)";
-  })
-
+  
   pig.addEventListener('mousemove', function({x, y}) {
     let transform = x > 0 && x < 114 ? "rotateY(0deg) " : "rotateY(180deg) ";
 
